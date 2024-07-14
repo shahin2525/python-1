6d1cbaa69ff6a87222d782629344da84bfda7668
shahin2525 <shahin451999@gmail.com>
2024-07-13 20:43:49 -0700

    practice function

--- hellow.py
+++ hellow.py
@@ -2,6 +2,71 @@
 #  print("Five is greater than two!")
 # if(5>2):
 #         print("Five is greater than two!")
-x = 5
-y = "Hello, World!"
-print(x)
+# x = 5
+# y = "Hello, World!"
+# print(x)
+# x = str(3)   
+# y = int(3)    
+# z = float(3)  
+# print(type(z))
+
+# _2myvar = "John"
+# my_ar = "John"
+# myvar = "John"
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+# x = "Python"
+# y = "is"
+# z = "awesome"
+# print(x, y, z)
+# x = "Python "
+# y = "is "
+# z = "awesome"
+# print(x + y + z)
+# x = 5
+# y = 10
+# print(x + y)
+# x = 5
+# y = "John"
+# print(x, y)
+# xdef = "awesome"
+
+#  myfunc():
+#   print("Python is " + x)
+
+# myfunc()
+# x = "awesome"
+
+# def myfunc():
+#   x = "fantastic"
+#   print("Python is " + x)
+
+# myfunc()
+# print("Python is " + x)
+# def myfunc():
+#   global x
+#   x = "fantastic"
+
+# myfunc()
+
+# print("Python is " + x)
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
