7e9e6ab3fd7bace67307070707df61f3a9f8e7db
shahin2525 <shahin451999@gmail.com>
2024-07-18 00:01:31 -0700

    practice intC:/Users/md shahin chowdhury/AppData/Local/Microsoft/WindowsApps/python3.12.exe

--- type.py
+++ type.py
@@ -1,18 +1,17 @@
 # complex
-x = 1j
+# x = 1j
 
-# name spcific
-y = list(("apple","orange"))
-# print(type(y))
-z = tuple(("rakib","sakib"))
+# # name spcific
+# y = list(("apple","orange"))
+# # print(type(y))
+# z = tuple(("rakib","sakib"))
 
-a = dict(name="rakib",age=20)
-b = dict(name="John", age=36)
+# a = dict(name="rakib",age=20)
+# b = dict(name="John", age=36)
 
-x = 1
-y = 35656222554887711
-z = -3255522
+# x = 1
+# y = 35656222554887711
+# z = -3255522
 
-print(type(x))
-print(type(y))
-print(type(z))
+c = -12.12
+print(type(c))
