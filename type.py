33328cb13bd4a9b9d060a59555af00552b5dda93
shahin2525 <shahin451999@gmail.com>
2024-07-17 23:59:31 -0700

    practice int

--- type.py
+++ type.py
@@ -5,7 +5,14 @@ x = 1j
 y = list(("apple","orange"))
 # print(type(y))
 z = tuple(("rakib","sakib"))
-print(type(z))
+
 a = dict(name="rakib",age=20)
 b = dict(name="John", age=36)
-print(type(b))
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
