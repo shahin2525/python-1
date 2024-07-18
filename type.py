436ef2b477fe472673c397e1b8088273524bfc4b
shahin2525 <shahin451999@gmail.com>
2024-07-17 23:57:07 -0700

    practice dist type

--- type.py
+++ type.py
@@ -3,4 +3,9 @@ x = 1j
 
 # name spcific
 y = list(("apple","orange"))
-print(type(y))
+# print(type(y))
+z = tuple(("rakib","sakib"))
+print(type(z))
+a = dict(name="rakib",age=20)
+b = dict(name="John", age=36)
+print(type(b))
